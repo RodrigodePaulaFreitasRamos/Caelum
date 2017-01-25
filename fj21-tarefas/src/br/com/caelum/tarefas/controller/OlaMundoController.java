26a56d71b99ced2be645f8dc09446039b2dff84a
web6600 <web6600@caelum55-15>
2017-01-25 21:08:16 -0200

    TAREFA

---
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/OlaMundoController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.caelum.tarefas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Rodrigo Ramos
+ *
+ */
+@Controller
+public class OlaMundoController {
+	@RequestMapping ("OlaMundoSpring")
+	public String execute(){
+		System.out.println("Executando a lógica com Spring MVC");
+		return "ok";
+	}
+
+}
